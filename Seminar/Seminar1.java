e8d9e203d7d71cd86bbd48d7d665e98f8a7c7d4d
kate <01redcat@mail.ru>
2023-04-22 19:33:29 +0300

    семинар 1 case switch, возврат ошибок

--- Seminar/Seminar1.java
+++ Seminar/Seminar1.java
@@ -5,28 +5,57 @@ package Seminar;
 //если искомый элемент не найден, метод вернет -2 в качестве кода ошибки.
 //если вместо массива пришел null, метод вернет -3
 
+
+import java.util.Scanner;
+
 public class Seminar1 {
     public static void main(String[] args) {
-        int [] array = {1,2,3,4};
-        System.out.println(findinArray(array, 10));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите длину массива не менее 5: ");
+        int arrLength = input.nextInt();
+        int [] array = new int[arrLength];
+        System.out.println("Введите элементы массива через enter: ");
+        for (int index = 0; index < arrLength; index++) {
+            array[index] = input.nextInt();
+        }
+        
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+        input.close();
+        
+        int res = (findinArray(array, 1));
+        switch (res) {
+            case -1:
+                
+                System.out.println("Длина массива менее 5");
+
+            case -2:
+                
+                System.out.println("искомый элемент не найден");
+            
+            case -3:
+                
+                System.out.println("вместо массива пришел null");    
+            
+            default:
+            System.out.printf("индекс искомого элемента %d", res);
+        }
     }
 
     public static int findinArray(int [] arr, int a) {
-        int find = 0;
-        try {
-            for (int index = 0; index < arr.length; index++) {
-                if (arr[index] == a) {
-                    find = index;
-                }
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
+        int minLength = 6;
+        if (arr.length < minLength) {
+            return -1;
         }
-        return find;
-        
-         
-        
-        
+        else if (arr == null){
+            return -3;
+        }
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] == a) {
+                return index;
+            }  
+        }
+        return -2;     
     }
 }
